8139d0f6810a3c6d71679d5b90d74d5623f9e758
SahaPylypyuk <sashapulupyk@gmail.com>
2020-02-18 14:43:12 +0200

    Updates

---
+++ precache-manifest.61d0c7fdb9c0a05848bb2f7fe3cd3b56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ea693159a008d5610f536a3dc93ae5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6cc1a58d06a59ece19be",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "9d26ae3e5c76775cd851",
+    "url": "./static/js/2.bfab6ca1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.bfab6ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cc1a58d06a59ece19be",
+    "url": "./static/js/main.e609c4f8.chunk.js"
+  },
+  {
+    "revision": "63d63249b359e20c019c",
+    "url": "./static/js/runtime-main.74d63094.js"
+  }
+]);
